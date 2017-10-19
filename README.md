d3c664a00b784023ab0d95bc949d612f197e83ef
Tony Tung <ttung@chanzuckerberg.com>
2017-10-18 17:47:12 -0700

    library-ize the module

---
+++ README.md
@@ -0,0 +1,7 @@
+# cloud-blobstore
+
+This library provides an abstraction layer for the most basic functionality provided by the cloud providers.  These are the basic functions required by the [Human Cell Atlas](https://www.humancellatlas.org/) [Data Storage Service](https://github.com/HumanCellAtlas/data-store).
+
+#### Running tests
+
+Run `make test` in the top-level directory.
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os, glob
+from setuptools import setup, find_packages
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+
+setup(
+    name="cloud-blobstore",
+    version="0.0.3",
+    url='https://github.com/chanzuckerberg/cloud-blobstore',
+    license='Apache Software License',
+    author='Human Cell Atlas contributors',
+    author_email='tonytung@chanzuckerberg.com',
+    description='Abstraction layer for cloud blobstores.',
+    long_description=open('README.md').read(),
+    install_requires=install_requires,
+    extras_require={},
+    packages=find_packages(exclude=['tests']),
+    platforms=['MacOS X', 'Posix'],
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
