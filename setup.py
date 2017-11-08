e3d1476809d2a2632e92234a9afc60e3b6399234
Tony Tung <ttung@chanzuckerberg.com>
2017-11-08 00:06:51 -0800

    0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 
 setup(
     name="cloud-blobstore",
-    version="0.0.5",
+    version="0.0.6",
     url='https://github.com/chanzuckerberg/cloud-blobstore',
     license='Apache Software License',
     author='Human Cell Atlas contributors',
